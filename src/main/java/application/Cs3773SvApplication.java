cb006e7bf4b2470f6a8c217afd6e48d78998843f
Austen Green <Austen782@gmail.com>
2022-07-27 13:30:38 -0500

    Fixed Api config issue

--- src/main/java/application/Cs3773SvApplication.java
+++ src/main/java/application/Cs3773SvApplication.java
@@ -1,13 +1,11 @@
-package com.example.cs3773sv;
+package application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Cs3773SvApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(Cs3773SvApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(Cs3773SvApplication.class, args);
+    }
 }
