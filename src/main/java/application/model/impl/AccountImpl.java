970a4d863b650f09ef25fbf9fc2dfcadf0fcbf3b
Austen Green <Austen782@gmail.com>
2022-07-28 01:56:15 -0500

    Random iD generator fix and deleted toString method

--- src/main/java/application/model/impl/AccountImpl.java
+++ src/main/java/application/model/impl/AccountImpl.java
@@ -8,38 +8,20 @@ import java.util.Random;
 public class AccountImpl extends UserImpl {
     @Id
     private Long iD;
-    private final Random rand = new Random();
 
     public AccountImpl(String name, String userName, String password) {
         super(name, userName, password);
-        this.iD = rand.nextLong(20000 - 10000) + 10000;
+        this.iD = new Random().nextLong(20000 - 10000) + 10000;
     }
 
     public AccountImpl(String userName, String password) {
         super(userName, password);
-        this.iD = rand.nextLong(20000 - 10000) + 10000;
+        this.iD = new Random().nextLong(20000 - 10000) + 10000;
     }
 
     public AccountImpl() {
     }
 
-    /*@Override
-    public String toString() {
-        return """
-                name = %s
-                username = %s
-                password = %s
-                iD = %d
-                """, super.getName(),
-
-        return String.format("name = %s\nusername = %s\n" +
-                        "password = %s\n" +
-                        "iD = %d",
-                super.getName(),
-                super.getUsername(),
-                super.getPassword(),
-                getiD());
-    } */
 
     public Long getiD() {
         return iD;
